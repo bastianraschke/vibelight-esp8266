66c62fe60809f1e4b3b9c44dec4e5e2f56d82f26
Bastian Raschke <bastian.raschke@quartett-mobile.de>
2016-06-22 18:21:32 +0200

    Initial commit: Created XCode project

---
+++ src/Client (iOS)/VibeLight/VibeLight/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  VibeLight
+//
+//  Created by Bastian Raschke on 22.06.16.
+//  Copyright © 2016 Bastian Raschke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ src/Client (iOS)/VibeLight/VibeLight/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  VibeLight
+//
+//  Created by Bastian Raschke on 22.06.16.
+//  Copyright © 2016 Bastian Raschke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
