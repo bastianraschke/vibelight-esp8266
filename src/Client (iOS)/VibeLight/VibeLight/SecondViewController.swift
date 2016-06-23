8007d86e72a570073357024f6e010f3e59880130
Bastian Raschke <bastian.raschke@quartett-mobile.de>
2016-06-23 10:43:12 +0200

    Connected save button to ViewController

--- src/Client (iOS)/VibeLight/VibeLight/SecondViewController.swift
+++ src/Client (iOS)/VibeLight/VibeLight/SecondViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func saveButtonPressed(sender: UIButton) {
+        NSLog("Button pressed")
+    }
 
 }
-
